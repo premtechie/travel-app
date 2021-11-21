537ddc70d55cc446df67cc95eca7272495bf0632
Premkumar N <premkumar.n@highpeaksw.com>
2021-11-21 22:16:36 +0530

    gh-pages deploy issue 3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='travel-app'>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
